f7c4c1de09dbde6a4f8b10f53c9c23c4aab514db
Rabi Roshan <rabiroshan@Rabis-MacBook-Pro.local>
2019-11-25 09:33:33 +0530

    done

--- Ciphers/ceaser.py
+++ Ciphers/ceaser.py
@@ -1,25 +1,27 @@
-#Rabi Roshan
-#Ceaser Cypher
+# Rabi Roshan
+# Ceaser Cypher
 
-def ceaser(name,k1):
-	result=""
 
-	for i in range(len(name)):
-		ch=name[i]
-		if(ch.isupper()):
-			result=result+ chr( (ord(ch)+k1 -65)%26 +26 )
-		
-		if(ch.islower()):
-			result=result+ chr( (ord(ch)+k1 -97)%26 +97 )
+def ceaser(name, k1):
+    result = ""
 
-	return result
+    for i in range(len(name)):
+        ch = name[i]
+        if(ch.isupper()):
+            result = result + chr((ord(ch)+k1 - 65) % 26 + 65)
+
+        if(ch.islower()):
+            result = result + chr((ord(ch)+k1 - 97) % 26 + 97)
+
+    return result
 
 
 def main():
-	n=str(raw_input("Input the text Message:"))
-	key=int(raw_input("Input the Key:"))
+    n = str(raw_input("Input the text Message:"))
+    key = int(raw_input("Input the Key:"))
 
-	cypher=str( ceaser(n,key) )
-	print("Decrypted Message:" +cypher)
-	
+    cypher = str(ceaser(n, key))
+    print("Decrypted Message:" + cypher)
+
+
 main()
--- Multi Chat/chat_server.py
+++ Multi Chat/chat_server.py
@@ -4,7 +4,6 @@
 
 # Python program to implement server side of chat room.
 import socket
-import select
 import sys
 from thread import *
 
